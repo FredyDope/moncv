fe9bacd200c018a7389b2072619a06f51aef6940
frederic <fredydope@gmail.com>
2018-06-14 23:24:01 +0200

    clean + components + header + marerial UI

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        <header className="Header-header">
+          <h1 className="Header-title">Web Developpeur</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Header
